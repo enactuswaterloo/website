b98bf4d840af4c456fe808febf78635cb54a9c12
Bilal Akhtar <me@itsbilal.com>
2015-01-01 00:35:41 -0500

    Add logo

--- enactuswaterloo/urls.py
+++ enactuswaterloo/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^$', main.views.index, name="home"),
+    url(r'^$', index, name="home"),
 
-    url(r'^$', main.views.about_us, name="about_us"),
+    url(r'^$', about_us, name="about_us"),
 )
