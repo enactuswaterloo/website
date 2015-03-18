17c58c97d8e3c12cf4efa4285717bc97ec830a72
Bilal Akhtar <me@itsbilal.com>
2015-03-18 00:33:36 -0400

    Add mass approve option to admin

--- about/admin.py
+++ about/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from about.models import Person
 
 # Register your models here.
-admin.site.register(Person)
+def approve_person(modeladmin, request, queryset):
+	queryset.update(approved=True)
+
+approve_person.short_description = "Approve selected users"
+
+class PersonAdmin (admin.ModelAdmin):
+	list_display = ['user', 'position', 'approved']
+	ordering = ['-priority']
+	actions = [approve_person]
+
+admin.site.register(Person, PersonAdmin)
