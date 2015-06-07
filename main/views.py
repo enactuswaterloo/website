83c4e52079f34d5de25382a66c70e9d46d04e903
Mohamed <mmmoussa@uwaterloo.ca>
2015-06-07 01:05:40 -0400

    Set up contact and sponsor pages

--- main/views.py
+++ main/views.py
@@ -16,4 +16,16 @@ def sponsors(request):
     return render(request, "main/sponsors.html")
 
 def contact(request):
-    return render(request, "main/contact.html")
+    if request.method == 'POST':
+    	if ('name' in request.POST and 'email' in request.POST and 'message' in request.POST) and (request.POST['name'] != "" and request.POST['email'] != "" and request.POST['message'] != ""):
+    		if "@" not in request.POST['email'] or "." not in request.POST['email'] or len(request.POST['email']) < 5:
+    			return render(request, "main/contact.html", {"error": "Invalid email entered."})
+    		else:
+	    		# Send email
+	    		print "Sent email"
+	    		return render(request, "main/contact.html", {"success": "success"})
+    	else:
+    		return render(request, "main/contact.html", {"error": "Not all fields have been filled out."})
+    else:
+    	return render(request, "main/contact.html")
+
