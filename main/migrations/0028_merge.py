1bc27fde9df398cc41fc1d70dc0aa1261d2b365a
Bilal Akhtar <me@itsbilal.com>
2015-06-25 00:06:16 -0400

    Merge the two diverging migration branches

---
+++ main/migrations/0028_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0025_remove_coverpic_subtitle'),
+        ('main', '0027_auto_20150625_0000'),
+    ]
+
+    operations = [
+    ]
